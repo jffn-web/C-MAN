ef5cd60b3b8d3368c86ab6ee493cbfc45c5848c5
João Falcão <jffn@cesar.school>
2026-05-24 18:17:05 -0300

    adiciona busca do jogador no mapa

--- src/main.c
+++ src/main.c
@@ -1,8 +1,67 @@
 #include <stdio.h>
 
-int main() {
+    struct jogador{
+      
+      int linha;
+      int coluna;
+      int vida;  
+    };
+    
+    void ImprimirMapa(int mapa[5][5]){
+        
+        int i;
+        int j;
 
-    printf("Dungeon Escape\n");
+        for(i = 0; i < 5; i++){
+            
+            for(j = 0; j < 5; j++){
+                
+                printf("%d ",mapa[i][j]);
+            } 
 
-    return 0;
-}
+            printf("\n");
+        }
+    }    
+
+    void AcharJogador(struct jogador *player,int mapa[5][5]){
+
+        int i;
+        int j;
+
+        for(i = 0; i < 5; i++){
+
+            for(j = 0; j < 5; j++){
+
+                if(mapa[i][j] == 2){
+
+                    player->linha = i;
+
+                    player->coluna = j;
+
+                }
+            }
+        }
+
+    }
+        
+
+    int main(){
+
+        struct jogador player;
+
+        int Mapa[5][5] = {
+            {1, 1, 1, 1, 1},
+            {1, 2, 0, 3, 1},
+            {1, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1}
+            };
+
+        ImprimirMapa(Mapa);
+
+        AcharJogador(&player,Mapa);
+        printf("Jogador na linha %d e coluna %d\n", player.linha, player.coluna);
+
+        
+        return 0;
+    }
