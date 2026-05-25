aaff0f8748d72f3e9e9fa7d79143cc0d3fb51257
João Falcão <jffn@cesar.school>
2026-05-25 14:57:59 -0300

    adiciona sistema de itens e pontuacao

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
       int linha;
       int coluna;
       int vida;  
+      int pontos;
     };
     
     void ImprimirMapa(int mapa[5][5]){
@@ -46,42 +47,49 @@
 
     void MoverJogador(struct jogador *player, int mapa[5][5], char tecla){
 
-        if(tecla == 'd' && mapa[player->linha][player->coluna + 1] != 1){
+        int novaLinha = player->linha;
+        int novaColuna = player->coluna;
 
-            mapa[player->linha][player->coluna] = 0;
-            player->coluna++;
-            mapa[player->linha][player->coluna] = 2;
-
-        }else if(tecla == 'a' && mapa[player->linha][player->coluna - 1] != 1){
-
-            mapa[player->linha][player->coluna] = 0;
-            player->coluna--;
-            mapa[player->linha][player->coluna] = 2;
-
-        }else if(tecla == 'w' && mapa[player->linha - 1][player->coluna] != 1){
-
-            mapa[player->linha][player->coluna] = 0;
-            player->linha--;
-            mapa[player->linha][player->coluna] = 2;
-
-        }else if(tecla == 's' && mapa[player->linha + 1][player->coluna] != 1){
-
-            mapa[player->linha][player->coluna] = 0;
-            player->linha++;
-            mapa[player->linha][player->coluna] = 2;
+        if(tecla == 'd'){
+            novaColuna++;
         }
+        else if(tecla == 'a'){
+            novaColuna--;
+        }
+        else if(tecla == 'w'){
+            novaLinha--;
+        }
+        else if(tecla == 's'){
+            novaLinha++;
+        }
+
+        if(mapa[novaLinha][novaColuna] != 1){
+
+            if(mapa[novaLinha][novaColuna] == 4){
+                player->pontos += 10;
+        }
+
+            mapa[player->linha][player->coluna] = 0;
+
+            player->linha = novaLinha;
+            player->coluna = novaColuna;
+
+            mapa[player->linha][player->coluna] = 2;
     }
+}
         
 
     int main(){
 
         struct jogador player;
+        player.vida = 3;
+        player.pontos = 0;
 
         int Mapa[5][5] = {
             {1, 1, 1, 1, 1},
             {1, 2, 0, 3, 1},
             {1, 0, 1, 0, 1},
-            {1, 0, 0, 0, 1},
+            {1, 4, 0, 0, 1},
             {1, 1, 1, 1, 1}
             };
         char tecla = ' ';
@@ -89,6 +97,7 @@
     while(tecla != 'q'){
 
         ImprimirMapa(Mapa);
+        printf("\nVida: %d | Pontos: %d\n", player.vida, player.pontos);
 
         AcharJogador(&player, Mapa);
 
